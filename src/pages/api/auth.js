b60b83509d3e859637df4da6cc63dfcfdc729aa0
Sagar Harale <105268941+sagarharale9@users.noreply.github.com>
2025-03-02 21:15:43 +0530

    Update auth.js

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -6,8 +6,8 @@ export default function handler(req, res) {
     const { username, password } = req.body;
   
     // Hardcoded Admin Credentials (Change this in production)
-    const ADMIN_USERNAME = "admin";
-    const ADMIN_PASSWORD = "password0330";
+    const ADMIN_USERNAME = "123";
+    const ADMIN_PASSWORD = "0330";
   
     if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
       res.status(200).json({ success: true });
